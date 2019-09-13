ae76ce4750e10f29933dbc47310500e3c92bd7cc
Joe <josephc.lambert@gmail.com>
2019-09-13 09:23:10 -0700

    replaces stubbed data with api response from SWAPI

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,17 +2,30 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import CardList from './components/CardList';
-import people from './api/stubPeople';
 import { peopleUrl } from './api/urls';
 
-console.log(people, peopleUrl);
-
 const App = () => {
-  const [peoples, setPeople] = useState(people);
+  const [people, setPeople] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(peopleUrl)
+      .then(response => {
+        const { results } = response.data;
+        console.log(results);
+        setPeople(people => [...people, ...results]);
+        setIsLoading(isLoading => !isLoading);
+      })
+      .catch(error => {
+        console.log('I have a bad feeling about this');
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className='App'>
       <h1 className='Header'>React Wars</h1>
-      <CardList people={people} />
+      {!isLoading && <CardList people={people} />}
     </div>
   );
 };
--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ people }) => {
+  console.log(people);
   const listPeople = people.map((person, key) => <Card key={key} person={person} />);
   return listPeople;
 };
