beaaa929057ed182c13fe72a69b5a37624a46580
Joe <josephc.lambert@gmail.com>
2019-09-13 09:28:06 -0700

    adds styled components. adds reset css values to index.css. adds CardContainer

--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Card from './Card';
+import styled from 'styled-components';
+
+const CardContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 const CardList = ({ people }) => {
   console.log(people);
   const listPeople = people.map((person, key) => <Card key={key} person={person} />);
-  return listPeople;
+  return (
+    <CardContainer>
+      <ul>{listPeople}</ul>
+    </CardContainer>
+  );
 };
 
 export default CardList;
